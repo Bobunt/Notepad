6dcfad79cffe7cab539cb4219d1d8616878497c4
Musta <daniil.cherrnyi@gmail.com>
2023-03-17 09:39:24 +0300

    Переход в main fragment

--- app/src/main/java/com/example/notepad/MainActivity.kt
+++ app/src/main/java/com/example/notepad/MainActivity.kt
@@ -6,6 +6,9 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()//Скрываем верхнюю плашку
         setContentView(R.layout.activity_main)
+
+        Router.showMainFragmentMain(supportFragmentManager)
     }
 }
--- app/src/main/java/com/example/notepad/MainFragment.kt
+++ app/src/main/java/com/example/notepad/MainFragment.kt
@@ -1,4 +1,26 @@
 package com.example.notepad
 
-class mainFragment {
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+
+class MainFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.main_fragment, container, false)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) = MainFragment()
+    }
 }
